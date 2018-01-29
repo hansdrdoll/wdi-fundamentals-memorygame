c3d4f4c60d83347f18abc927cbaf219198a31e96
Hans Kirkendoll <hanskirkendoll@Jeremy-Paredess-MacBook-Pro.local>
2018-01-28 21:35:02 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+	alert("Sorry, not equal");
+}}
