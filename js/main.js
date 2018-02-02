b850152f3827e06b17e505536b3578a2b0af35b7
Hans Kirkendoll <hanskirkendoll@Jeremy-Paredess-MacBook-Pro.local>
2018-02-01 20:43:53 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,19 +2,26 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped " + cardsInPlay);
-
-if (cardsInPlay.length === 2) {
+//note that instructions change this from alert to consolelog
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-	alert("Sorry, not equal");
-}}
+			console.log("You found a match!");
+			} else {
+			console.log("Sorry, not equal");
+		}
+};
+
+var flipCard = function(cardID) {
+	//Display card user selected
+	console.log("user flipped " + cards[cardID]);
+	//Add that card to inPlay array
+	cardsInPlay.push(cardID);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+flipCard(0);
+
+flipCard(2);
