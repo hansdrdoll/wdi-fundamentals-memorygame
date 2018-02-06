11172faa97330e01c9497efafa68a2a8c573567d
Hans Kirkendoll <hanskirkendoll@Jeremy-Paredess-MacBook-Pro.local>
2018-02-06 11:37:01 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+	},
+	{	
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+	},
+	{	
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
 //note that instructions change this from alert to consolelog
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match!");
+			alert("You found a match!");
 			} else {
-			console.log("Sorry, not equal");
+			alert("Sorry, not equal");
 		}
 };
 
 var flipCard = function(cardID) {
 	//Display card user selected
-	console.log("user flipped " + cards[cardID]);
+	console.log("user flipped " + cards[cardID].rank);
+	console.log("suit is " + cards[cardID].suit);
+	console.log(cards[cardID].cardImage);
 	//Add that card to inPlay array
-	cardsInPlay.push(cardID);
+	cardsInPlay.push(cards[cardID].rank);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
